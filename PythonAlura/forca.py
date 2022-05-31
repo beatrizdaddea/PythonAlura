54f79645262944ed9d146a08cea292d361e61764
Beatriz Daddea <beatrizchith@gmail.com>
2022-05-31 11:28:17 -0300

    Add files via upload

---
+++ PythonAlura/forca.py
@@ -0,0 +1,9 @@
+
+def jogar_forca():
+
+    print("*********************************")
+    print("** Bem vindo ao jogo de Forca! **")
+    print("*********************************")
+
+if (__name__ == "__main__"):
+    jogar_forca()
