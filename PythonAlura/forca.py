6ff2f10abc143a8c087634a7deaad0a2cb076f19
Beatriz Daddea <beatrizchith@gmail.com>
2022-06-09 11:24:37 -0300

    Delete PythonAlura directory

--- PythonAlura/forca.py
+++
@@ -1,9 +0,0 @@
-
-def jogar_forca():
-
-    print("*********************************")
-    print("** Bem vindo ao jogo de Forca! **")
-    print("*********************************")
-
-if (__name__ == "__main__"):
-    jogar_forca()
